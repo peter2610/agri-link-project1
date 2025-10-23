5b5e63bb0ad024e87bc6f73cb921c54a96c8459c
peter munyambu <petermunyambu1000@gmail.com>
2025-10-23 21:50:38 +0300

    reorder imports in config.py for better readability

--- agri-link-backend/config.py
+++ agri-link-backend/config.py
@@ -1,10 +1,9 @@
 from flask import Flask
-from flask_migrate import Migrate
-from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from flask_restful import Api
+from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
-
+from sqlalchemy import MetaData
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -18,5 +17,4 @@ db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 db.init_app(app)
-
 api = Api(app)
