1840a9a0b081160e82ba16e4a4f8a773ad84d0ef
peter munyambu <petermunyambu1000@gmail.com>
2025-10-23 21:50:23 +0300

    refactor app.py to improve structure and clarity

--- agri-link-backend/app.py
+++ agri-link-backend/app.py
@@ -1,12 +1,16 @@
-# from flask import request, session
 from flask_cors import CORS
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from config import app, api
 from routes.main_route import Main
+from routes.offer_route import OfferResource
 
+# Enable CORS
+CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://127.0.0.1:3000"]}})
 
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://127.0.0.1:3000"]}})    
-
+# Register RESTful resources
 api.add_resource(Main, '/', endpoint='main')
+api.add_resource(OfferResource, '/offer', endpoint='offer')
 
 if __name__ == '__main__':
-    app.run( port=5555, debug=True)
+    app.run(host='127.0.0.1', port=5555, debug=True)
